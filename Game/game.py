dd5c050d578909075cca3cd67d6ed51e70d76713
Solrak <luiscarlosquesada@gmail.com>
2022-07-12 01:13:38 -0600

    Player movement

--- Game/game.py
+++ Game/game.py
@@ -4,15 +4,23 @@ import sys
 from enemy import Enemy
 from player import Player
 
+# Setup
 SCREEN_SIZE = (640, 480)
 FPS = 60
 
 pg.init()
+
 clock = pg.time.Clock()
+icon = pg.image.load('Assets/green.png')
+pg.display.set_caption('Space Invaders')
+pg.display.set_icon(icon)
+
 screen = pg.display.set_mode(SCREEN_SIZE)
 
-
-
+# Player setup
+player = Player((SCREEN_SIZE[0] / 2, 460), 'Assets/player.png')
+player_group = pg.sprite.Group()
+player_group.add(player)
 
 # Refactorizar la generación de enemigos
 e1 = Enemy((100, 200), 'Assets/orange.png')
@@ -21,20 +29,13 @@ e3 = Enemy((300, 200), 'Assets/purple.png')
 e4 = Enemy((400, 200), 'Assets/red.png')
 e5 = Enemy((500, 200), 'Assets/yellow.png')
 
-
+# Al chile...
 enemy_group = pg.sprite.Group()
 enemy_group.add(e1)
 enemy_group.add(e2)
 enemy_group.add(e3)
 enemy_group.add(e4)
 enemy_group.add(e5)
-
-
-
-# Refactorizar la generación de player
-player = Player((240, 300), 'Assets/player.png')
-player_group = pg.sprite.Group()
-player_group.add(player)
 
 
 # Graphics update, kinda like a global draw
@@ -50,6 +51,9 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+
+        player.controller(pg.key.get_pressed())
 
     screen.fill((0, 0, 0))
     draw_state()
--- Game/player.py
+++ Game/player.py
@@ -1,20 +1,56 @@
 import pygame as pg
+from pygame.locals import *
 
 class Player(pg.sprite.Sprite):
 
-    def __init__(self, pos, image, scale=(32, 32)):
+    def __init__(self, pos, image, scale=(32, 32), limits = (640, 480)):
         super().__init__()
 
+        # Sprite
+        (self.wl, self.hl) = limits
+        (self.w, self.h) = scale
         self.image = pg.image.load(image).convert_alpha()
         self.image = pg.transform.scale(self.image, scale)
         self.rect = self.image.get_rect(center=pos)
 
-        self.move = 0
+        # Movement
+        self.velocity = 5
+        self.movement = 0
+        self.max_speed = 10
+    
+    def move(self, velocity):
+        self.movement += velocity
+        self.movement = max(-self.max_speed, min(self.movement, self.max_speed))
+        
 
-
-    def control(self, x):
-        self.movex += x
+    def shoot():
+        pass
 
 
     def update(self):
-        self.rect.x = self.rect.x + 1
+        
+        if (self.rect.x + self.movement) < 0:
+            self.rect.x = self.w/2
+
+        elif (self.rect.x + self.movement) > self.wl:
+            self.rect.x = self.wl - self.w
+        
+        else:
+            self.rect.x += self.movement
+        
+
+
+    def controller(self, keystate):
+        if keystate[pg.K_a]:
+            self.move(-self.velocity)
+        
+        if keystate[pg.K_d]:
+            self.move(self.velocity)
+
+        # Shoot
+        if keystate[pg.K_SPACE]:
+            self.move(-self.step)
+
+        if not any(keystate):
+            self.movement = 0
+           
