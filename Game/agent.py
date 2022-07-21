10cb0652b7f5be79ce3f65fe16d91ed434fdcc64
Solrak <luiscarlosquesada@gmail.com>
2022-07-20 23:12:11 -0600

    AI agent update CNN

--- Game/agent.py
+++ Game/agent.py
@@ -7,64 +7,73 @@ import torch
 
 class Agent():
     def __init__(self):
-        
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.action_map = [Actions.MOVE_LEFT, Actions.MOVE_RIGHT, Actions.SHOOT]
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.action_map = [Actions.MOVE_LEFT,
+                           Actions.MOVE_RIGHT, Actions.SHOOT]
         self.last_state = None
 
         self.prediction_network = DQAgent().to(self.device)
         self.target_network = DQAgent().to(self.device)
-        
+
         '''
         self.PATH = './agent.pth'
         torch.save(model.state_dict(), PATH)
-        pass
         '''
 
-
+    # Actualiza el agente cuando se obtiene un fitting aceptable
     def update_agent():
         pass
-    
+
+    # Recibe el estado
 
     def capture_state(self, state):
         img = Image.frombytes('RGB', (640, 480), state, 'raw')
-        self.last_state = np.array(img)
-        
+        img_array = np.array(img).reshape((3, 640, 480))
+        self.last_state = torch.tensor(img_array).to(
+            self.device).type(torch.float32).unsqueeze(dim=0)
         pass
 
-    
-    def reward():
+    # torch.cuda.IntTensor
+
+    # Recibe el reward y ejecuta la corrección de error
+    def reward(self, reward):
         pass
 
+    # PRedice el estado y ejecuta una acción
 
     def get_action(self):
         self.prediction_network(self.last_state)
 
         return Actions.SHOOT
-        
 
 
-
+# Red neuronal del agente
 class DQAgent(nn.Module):
     def __init__(self):
         super(DQAgent, self).__init__()
-        self.feature_extractor = []
-        self.feature_extractor.append(nn.Conv2d(in_channels = 3, out_channels= 6, kernel_size = 5))
-        self.feature_extractor.append(nn.LeakyReLU())
-        self.feature_extractor.append(nn.MaxPool2d(2, 2))
 
-        self.classificator = []
-        self.classificator.append(nn.Linear(17 * 5 * 5, 144))
-    
+        filters = [3, 12, 24, 48, 96, 194]
+
+        self.feature_extraction = []
+
+        for idx in range(1, len(filters)):
+            self.feature_extraction.append(nn.Conv2d(
+                in_channels=filters[idx - 1], out_channels=filters[idx], kernel_size=5))
+
+        self.feature_activator = nn.LeakyReLU()
+        self.feature_pool = nn.MaxPool2d(2, 2)
+
+        #self.activation_final = nn.Linear(17 * 5 * 5, 144)
+
     def forward(self, x):
-        for layer in self.feature_extractor:
-            x = layer(x)
 
-        x = x.view()
-
-        for layers in self.classificator:
+        for layer in self.feature_extraction:
             x = layer(x)
+            x = self.feature_activator(x)
+            x = self.feature_pool(x)
 
         return x
 
--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 ### Estudiantes:
  - Luis Carlos Quesada Rodríguez
  - Gianfranco Bagnarello Hernández 
-#
+# 
 ### Descripción del proyecto:
 Consiste en un simulador del juego "space invaders" donde el jugador se enfrenta a varias oleadas de enemigos, y debe dispararles para eliminarlas y obtener puntos. 
 El simulador busca que un agente de Q-Learning logre aprender a jugar y ganar.
+
 
 ### Clases implementadas:
 - Actions: contiene las acciones posibles que pueden ser tomadas en el juego.
@@ -19,14 +20,12 @@ El simulador busca que un agente de Q-Learning logre aprender a jugar y ganar.
 - Enemy: contiene propiedades de los enemigos individualmente, los tipos de enemigos, y sus interacciones con el ambiente.
 - Game manager: controla el juego importando clases y corriendo el agente.
 - Game: Importa clases de entidades para dibujar el juego y correr el game loop.
-- Player: 
+- Player: clase que controla el movimiento e interacciones de la nave del agente con el ambiente.
 
 ### Resultados:
 - Pruebas realizadas con 10 oleadas:
 - Pruebas realizadas con 100 oleadas:
-- Gráficos:
+- Figuras:
 
 Bibliotecas requeridas:
-- pygame
-- numpy
-- torch
+- Se encuentran en el archivo requirements.txt adjunto
